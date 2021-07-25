d9916f1b0ecea20d5e9965aac500a2418922d2e6
Bogdan <bogdan.p.deac@gmail.com>
2021-07-25 14:50:16 +0300

    add DAC control based on serial input

--- src/dac_control/dac_control.ino
+++ src/dac_control/dac_control.ino
@@ -1,3 +1,6 @@
+#include <Wire.h>
+#include <Adafruit_MCP4725.h>
+
 #define ZERO  48
 #define ONE   49
 #define TWO   50
@@ -6,9 +9,12 @@
 #define FIVE  53
 
 int g_incoming_byte = 0; // for incoming serial data
+Adafruit_MCP4725 dac;
 
 void setup() {
   Serial.begin(9600); // opens serial port, sets data rate to 9600 bps
+
+  dac.begin(0x62);
 }
 
 void loop() {
@@ -21,31 +27,37 @@ void loop() {
       case ZERO: {
         Serial.print("I received: ");
         Serial.println(g_incoming_byte, DEC);
+        dac.setVoltage(0, false);
         break;
       }
       case ONE: {
         Serial.print("I received: ");
         Serial.println(g_incoming_byte, DEC);
+        dac.setVoltage(1, false);
         break;
       }
       case TWO: {
         Serial.print("I received: ");
         Serial.println(g_incoming_byte, DEC);
+        dac.setVoltage(2, false);
         break;
       }
       case THREE: {
         Serial.print("I received: ");
         Serial.println(g_incoming_byte, DEC);
+        dac.setVoltage(3, false);
         break;
       }
       case FOUR: {
         Serial.print("I received: ");
         Serial.println(g_incoming_byte, DEC);
+        dac.setVoltage(4, false);
         break;
       }
       case FIVE: {
         Serial.print("I received: ");
         Serial.println(g_incoming_byte, DEC);
+        dac.setVoltage(5, false);
         break;
       }
       default: {
